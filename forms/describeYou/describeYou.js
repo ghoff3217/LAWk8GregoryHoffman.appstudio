589785a7173b9002513436c59eca8203079995f2
Gregory Hoffman <gah32927@creighton.edu>
2021-04-05 14:21:02 -0500

    code

--- forms/describeYou/describeYou.js
+++ forms/describeYou/describeYou.js
@@ -1,5 +1,18 @@
 
+rdoPersonality.onchange=function(){
+  let userChoice = $("input[name=rdoPersonality]:checked").prop("value")
+   console.log(`You picked ${userChoice}`)
+   
+  if (rdoPersonality.value == 0)   // picked the first choice
+      lblRdoOutput.value = `I would agree that you are a ${userChoice} person too!`
+   else if (rdoPersonality.value == 1)
+      lblRdoOutput.value = `I would agree that you are a ${userChoice} person too!`
+    else if (rdoPersonality.value == 2)
+      lblRdoOutput.value = `I would agree that you are a ${userChoice} person too!`
+    else if (rdoPersonality.value == 3)
+      lblRdoOutput.value = `I would agree that you are a ${userChoice} person too!`
+}
 
-
-
-
+btnChangeForm1.onclick=function(){
+  ChangeForm(favExercises)
+}
